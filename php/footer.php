31e85b2c8ed3f16553699a5d572300fa9a9d1ecf
jacrifla <jaquelini_cris@hotmail.com>
2022-10-20 19:43:29 -0300

    Alteracoes para php

---
+++ php/footer.php
@@ -0,0 +1,44 @@
+<footer id="footer" class="bg-banner">
+    <h3 class="text-light">Mapa do Site</h3>
+    <nav class="navbar navbar-dark">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="cidadania.php">Cidadania</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tripartite.php">Executivo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="presidente.php">Presidente</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="senado.php">Senadores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cargos.php">Cargos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="partidos.php">Partidos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="linha_do_tempo.php">Linha do Tempo - Presidentes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="galeria-juizes.php">Galeria - Juízes do STF</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contato.php">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="referencias.html">Referências</a>
+                </li>
+            </ul>
+    </nav>
+    <div class="row text-center">
+        <small class="text-light">© 2022 - <a class="link-light" href="contato.php">Ursinhos Felizes em Chamas</a> - Todos os direitos reservados</small>
+    </div>
+    <div class="row text-center">
+        <small class="text-light">Clique aqui <a href="normas-de-uso.php"><img class="img-fluid" src="imagens/120069.png" alt="imagem das normas de uso"></a> veja nossas normas de
+            uso</small>
+    </div>
+</footer>
---
+++ php/header.php
@@ -0,0 +1,25 @@
+<header class="bg-banner text-center">
+    <img class="img-fluid" src="imagens/portal-intelecto.png" alt="Banner">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-banner">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="imagens/icon-menu48x48.png" alt="Logo do site"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item"><a class="nav-link" aria-current="page" href="#">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Cidadania</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Cargos</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Tripartite</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Candidatos</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Candidaturas</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Partidos</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Galeria Juízes</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Linha do Tempo</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+</header>
