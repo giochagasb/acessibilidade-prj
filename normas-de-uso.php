dbcbe9932caf35e73ad7689bb0b756a77062aed0
jacrifla <jaquelini_cris@hotmail.com>
2022-10-20 21:11:30 -0300

    alteracoes na paginas

--- normas-de-uso.php
+++ normas-de-uso.php
@@ -10,6 +10,8 @@
     <title>Normas de Uso</title>
 </head>
 <body>
+  <?php include_once('php/header.php'); ?>
+
     <div class="container">
 
         <article class="p-3 my-5">
@@ -81,7 +83,12 @@
                 </div>
             </div>
         </article>
-
+        
     </div>
+    <?php include_once('php/footer.php'); ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 </body>
 </html>
