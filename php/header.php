fe2d9fd2257c590446a0ca104b725d4205524225
jacrifla <jaquelini_cris@hotmail.com>
2022-11-27 02:38:20 -0300

    add link de pagina inicial

--- php/header.php
+++ php/header.php
@@ -1,7 +1,7 @@
 <header class="bg-banner text-center">
     <nav class="navbar navbar-expand-lg navbar-dark bg-banner">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img src="imagens/icon-menu48x48.png" alt="Logo do site"></a>
+          <a class="navbar-brand" href="index.php"><img src="imagens/icon-menu48x48.png" alt="Logo do site"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
