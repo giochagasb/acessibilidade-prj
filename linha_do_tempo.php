c8a96350984681f288153c7609fe9ece88b8caf1
jacrifla <jaquelini_cris@hotmail.com>
2022-11-27 02:52:50 -0300

    Add favicon

--- linha_do_tempo.php
+++ linha_do_tempo.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <link rel="shortcut icon" href="imagens/icon-menu48x48.png" type="image/x-icon">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
